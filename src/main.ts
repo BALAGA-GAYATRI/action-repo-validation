aefb511883792101b470ec1a29eded10b11f6bf3
Balaga Gayatri <balaga-gayatri@github.com>
2021-07-13 17:49:51 +0530

    all api

--- src/main.ts
+++ src/main.ts
@@ -62,6 +62,8 @@ async function main() {
 		validationResult.push(validationResultRepo);
 	}
 	console.log(JSON.parse(JSON.stringify(validationResult)))
+	core.setOutput("validationResult", JSON.parse(JSON.stringify(validationResult)));
+
 }
 
 main();
